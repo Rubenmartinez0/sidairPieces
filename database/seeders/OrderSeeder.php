8c29a18cff60ed95b10e48de061308a3efa176fb
Ruben Martinez <you@example.com>
2021-01-18 16:08:56 +0100

    little changes

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -14,20 +14,23 @@ class OrderSeeder extends Seeder
     public function run()
     {
         $orders = [
-            ['order_id' => 2001140001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101140001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-14 14:38:17", 'updated_at' => "2021-01-14 14:38:17"],
-            ['order_id' => 2001140002, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101140002, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-14 15:38:17", 'updated_at' => "2021-01-14 15:38:17"],
-            ['order_id' => 2001140003, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101140003, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-14 16:38:17", 'updated_at' => "2021-01-14 16:38:17"],
 
-            ['order_id' => 2001150001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101150001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-15 14:38:17", 'updated_at' => "2021-01-15 14:38:17"],
-            ['order_id' => 2001150002, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101150002, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-15 15:38:17", 'updated_at' => "2021-01-15 15:38:17"],
 
-            ['order_id' => 2001180001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            ['order_id' => 2101180001, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
             'state_id' => 1, 'created_at' => "2021-01-18 13:38:17", 'updated_at' => "2021-01-18 13:38:17"],
+            ['order_id' => 2101180002, 'client_id' => 1, 'project_id' => 1, 'created_by' => 1,'material_id' => 1,
+            'state_id' => 1, 'created_at' => "2021-01-18 15:38:17", 'updated_at' => "2021-01-18 15:38:17"],
+            
             
         ];
         foreach($orders as $order){
