e0ff433d588d48b84ae570de18f954961c20db0f
Ruben Martinez <you@example.com>
2021-01-26 10:51:37 +0100

    added measurements to seeder

--- database/seeders/CartItemSeeder.php
+++ database/seeders/CartItemSeeder.php
@@ -14,23 +14,13 @@ class CartItemSeeder extends Seeder
     public function run()
     {
         $cartItems = [
-            ['user_id' => 1, 'type_id' => 1, 'quantity' => 1],
-            ['user_id' => 1, 'type_id' => 2, 'quantity' => 10],
-            ['user_id' => 1, 'type_id' => 3, 'quantity' => 3],
-            ['user_id' => 1, 'type_id' => 4, 'quantity' => 5],
-            // ['user_id' => 1, 'type_id' => 4, 'quantity' => 6],
-            // ['user_id' => 1, 'type_id' => 2, 'quantity' => 3],
-            // ['user_id' => 1, 'type_id' => 5, 'quantity' => 2],
-            // ['user_id' => 1, 'type_id' => 1, 'quantity' => 1],
-            // ['user_id' => 1, 'type_id' => 2, 'quantity' => 10],
-            // ['user_id' => 1, 'type_id' => 3, 'quantity' => 3],
-            // ['user_id' => 1, 'type_id' => 4, 'quantity' => 5],
-            // ['user_id' => 1, 'type_id' => 4, 'quantity' => 6],
-            // ['user_id' => 1, 'type_id' => 2, 'quantity' => 3],
-            // ['user_id' => 1, 'type_id' => 5, 'quantity' => 2],
+            ['user_id' => 1, 'type_id' => 1, 'quantity' => 1, 'measurements' => 'a:1,b:2,c:4,e:6,l:1,x:3,y:5'],
+            ['user_id' => 1, 'type_id' => 2, 'quantity' => 10, 'measurements' => 'a:4,b:4,d:4,e:4,f:4,l:4,r:4'],
+            ['user_id' => 1, 'type_id' => 3, 'quantity' => 3, 'measurements' => 'a:5,b:5,c:55,e:5,f:555,r:5,w:5'],
+            ['user_id' => 1, 'type_id' => 4, 'quantity' => 5, 'measurements' => 'a:66,b:6,c:6,d:666,h:6,l1:6,m:6,n:6,r1:6,r2:6'],
 
-            ['user_id' => 2, 'type_id' => 3, 'quantity' => 1],
-            ['user_id' => 2, 'type_id' => 4, 'quantity' => 1],
+            ['user_id' => 2, 'type_id' => 3, 'quantity' => 1, 'measurements' => 'a:5,b:5,c:55,e:5,f:555,r:5,w:5'],
+            ['user_id' => 2, 'type_id' => 4, 'quantity' => 1, 'measurements' => 'a:66,b:6,c:6,d:666,h:6,l1:6,m:6,n:6,r1:6,r2:6'],
 
         ];
         foreach($cartItems as $item){
